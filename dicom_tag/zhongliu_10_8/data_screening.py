75f612d34ca5086cc93ed4eb5db0f1cf037a3754
liuxiangyang <liu761477028@gmail.com>
2019-10-08 22:11:00 +0800

    hongliu

--- dicom_tag/zhongliu_10_8/data_screening.py
+++ dicom_tag/zhongliu_10_8/data_screening.py
@@ -3,9 +3,34 @@ import shutil
 import pydicom
 from os.path import join, getsize
 
+from pydicom import dicomio
+import SimpleITK as sitk
+import numpy as np
+
+
+# dicomPath2 = r"E:\new_dir\1002480402170216154051\1.3.12.2.1107.5.1.4.58462.30000017021514463903100003011"
+def get_tag(dicomPath1,dicomPath2):
+    dcm = pydicom.dcmread(dicomPath1)  # 选择一个普通的非压缩dcm文件作为转换模板
+    image = sitk.ReadImage(dicomPath2)
+    image_array = sitk.GetArrayFromImage(image)
+    np_array = np.int16(image_array)
+    image_array = np.squeeze(np_array)  # 读取有压缩分段的dcm图像数据
+    dataset = pydicom.dcmread(dicomPath2)  # 用来修改Tag，图像读写相关的tag都要改
+    dcm.data_element('PixelSpacing').value = dataset.data_element('PixelSpacing').value
+    dcm.data_element('RescaleIntercept').value = dataset.data_element('RescaleIntercept').value
+    dcm.data_element('RescaleSlope').value = dataset.data_element('RescaleSlope').value
+    dcm.PixelData = image_array.tobytes()
+    dcm.save_as(dicomPath2)
+    dcm = dicomio.read_file(dicomPath2)
+    return dcm.StudyDescription
+
+
+
+dicomPath1 = r"E:\fuxing_idea\dicom\Li_Yan_3a85e_00274.dcm"
 
 path = r"E:\新建文件夹 (2)"
 del_path = r"E:\新建文件夹 (3)"
+save_path = r"E:\新建文件夹 (4)"
 file_list = os.listdir(path)
 size = 30
 print(file_list)
@@ -28,11 +53,16 @@ for i in range(len(file_list)):
 
     dicom_list = os.listdir(file_path)
     dicom_path = os.path.join(file_path,dicom_list[0])
-    dcm = pydicom.read_file(dicom_path)
-    kvp = dcm.KVP
+    targetDir = os.path.join(save_path,dicom_list[0])
+
+    shutil.copy(dicom_path, targetDir)
+
+    # dcm = pydicom.read_file(dicom_path)
+    StudyDescription = get_tag(dicomPath1,targetDir)
+    os.remove(targetDir)
     need_str1 = "chest"
     need_str2 = "thorax"
-    if need_str1 not in kvp and need_str2 not in kvp:
+    if need_str1 not in StudyDescription and need_str2 not in StudyDescription:
         shutil.move(file_path, save_path)
-    print(kvp)
+    print(StudyDescription)
 
